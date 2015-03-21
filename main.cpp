0b00835d1a16eac5467de51b6a259ea16ce464bf
Abhisek Pattnaik <abhisekp@engineer.com>
2015-03-21 15:39:30 +0530

    Set Window immovable

--- main.cpp
+++ main.cpp
@@ -8,36 +8,50 @@ namespace spooky {
 	int HEIGHT = 576;
 
 	void keyCallback(GLFWwindow*, int, int, int, int);
+	void positionCallback(GLFWwindow*, int, int);
 }
 
 int main(int argc, char *argv[])
 {
+	// Check GLFW initialization
 	if (glfwInit() != GL_TRUE) {
 		std::cout << "GLFW Initialization Failed." << std::endl;
 		return 0;
 	}
 	std::cout << "GLFW Initialized Successfully." << std::endl;
 
+	// Set window configurations
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);	
 
+	// Create a GLFW window
 	GLFWwindow *window = glfwCreateWindow(spooky::WIDTH, spooky::HEIGHT, "Spooky", nullptr, nullptr);
 	if (!window) {
 		std::cout << "GLFW Window Creation Failed" << std::endl;
 		return 0;
 	}
 	std::cout << "GLFW Window Creation Successful" << std::endl;
+	// Make the window as current context
 	glfwMakeContextCurrent(window);
 
+	// Check GLEW initialization
 	if (glewInit() != GLEW_OK) {
 		std::cout << "GLEW Initialized Failed." << std::endl;
 	}
 	std::cout << "GLEW Initialized Successfully" << std::endl;
 
+	// Set key callbacks
 	glfwSetKeyCallback(window, spooky::keyCallback);
+	// Set the postion callback to make the window immovable
+	glfwSetWindowPosCallback(window, spooky::positionCallback);
+	// Set the position to center
+	spooky::positionCallback(window, 0, 0);
 
+	// loop until closed
 	while (glfwWindowShouldClose(window) != GL_TRUE) {
+		// poll various kinds of events
 		glfwPollEvents();
 
+		// swap front and back buffers
 		glfwSwapBuffers(window);
 	}
 
@@ -51,4 +65,12 @@ void spooky::keyCallback(GLFWwindow *window, int key, int scancode, int action, 
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
 		glfwSetWindowShouldClose(window, GL_TRUE);
 	}
+}
+
+void spooky::positionCallback(GLFWwindow *window, int xPos, int yPos)
+{
+	const GLFWvidmode *mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+	int x = (mode->width - WIDTH) / 2;
+	int y = (mode->height - HEIGHT) / 2;
+	glfwSetWindowPos(window, x, y);
 }
