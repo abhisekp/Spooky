d58870e1036fa68570c62f89743a2b5374126e69
Abhisek Pattnaik <abhisekp@engineer.com>
2015-03-21 15:12:57 +0530

    GLEW Initialized Successfully

--- main.cpp
+++ main.cpp
@@ -11,18 +11,23 @@ namespace spooky {
 int main(int argc, char *argv[])
 {
 	if (glfwInit() != GL_TRUE) {
-		std::cout << "GLFW Initialization failed." << std::endl;
+		std::cout << "GLFW Initialization Failed." << std::endl;
 		return 0;
 	}
-	std::cout << "GLFW Successfully Initialized." << std::endl;
+	std::cout << "GLFW Initialized Successfully." << std::endl;
 
 	GLFWwindow *window = glfwCreateWindow(spooky::WIDTH, spooky::HEIGHT, "Spooky", nullptr, nullptr);
 	if (!window) {
-		std::cout << "GLFW window couldn't be created." << std::endl;
+		std::cout << "GLFW Window Creation Failed" << std::endl;
 		return 0;
 	}
-	std::cout << "GLFW window Successfully created." << std::endl;
+	std::cout << "GLFW Window Creation Successful" << std::endl;
 	glfwMakeContextCurrent(window);
+
+	if (glewInit() != GLEW_OK) {
+		std::cout << "GLEW Initialized Failed." << std::endl;
+	}
+	std::cout << "GLEW Initialized Successfully" << std::endl;
 
 	while (glfwWindowShouldClose(window) != GL_TRUE) {
 		glfwPollEvents();
